9a990665ba5916b92b9dc3d96be7b3ddfb564d5e
supercoolx <94512319+supercoolx@users.noreply.github.com>
2024-10-13 16:04:33 +0000

    fix: change telegram link

--- frontend/src/libs/constants.ts
+++ frontend/src/libs/constants.ts
@@ -15,7 +15,7 @@ export const GAME = {
 }
 
 export const LINK = {
-    TELEGRAM_MINIAPP: 'https://t.me/SmashMoleBot/play',
+    TELEGRAM_MINIAPP: 'https://t.me/eto_betting_bot/play',
     TELEGRAM_GROUP: 'https://t.me/test',
     TELEGRAM_CHANNEL: 'https://t.me/test',
     X: 'https://x.com/test?t=p9OEN2s6HczzGOlhMc2xyw&s=09',
